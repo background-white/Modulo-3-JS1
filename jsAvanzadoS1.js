122b7129c9bcd5e0da563d7cfad557c62a9f9a01
linkinkc <linkinkc@gmail.com>
2022-11-24 19:04:03 -0500

    actualizando del ejercicio 10 al 12 JS

--- jsAvanzadoS1.js
+++ jsAvanzadoS1.js
@@ -117,3 +117,50 @@ for(const prop in pokemons[0]){
   })
   ul.append(li);
 }
+
+//10. Utilizando javascript crear una tabla de pokemons con las siguientes columnas: id, name, type, base_damage, base_hp, speed
+ 
+const root = document.getElementById("root")
+const table = document.createElement("table")
+table.classList.add("default")
+table.setAttribute("border", "1")
+root.append(table)
+ 
+// HEADERS
+ 
+const tr = document.createElement("tr")
+ 
+table.append(tr)
+for(const prop in pokemons[0]){
+    const th = document.createElement("th")
+   
+    th.textContent = prop
+    th.style.cursor = "pointer"
+    th.addEventListener("click", () => {
+        sortPokemons(prop)
+        table.innerHTML = ""
+        table.append(tr)
+        createTableBody()
+    })
+    tr.append(th)
+}
+ 
+ 
+ 
+// TABLE BODY
+function createTableBody(){
+    for(const index in pokemons){
+        const trb = document.createElement("tr")
+        for(const prop in pokemons[index]){
+            const td = document.createElement("td")
+            td.textContent = pokemons[index][prop]
+            trb.append(td)
+        }
+        table.append(trb)
+    }
+}
+ 
+ 
+//11. Utilizando javascript modifica el codigo creado en el ejecicio anterior y agrega un evento que permita ordenar los pokemons haciendo click en sus encabezados.
+ 
+//12. Corrige la function sortPokemons para que acepte ordenarlos segun id y name.
